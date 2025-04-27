0bc72d5a9d013a6dd6129cbd91d4a598fc9d4641
delinschiivlad <vladelinschii05@gmail.com>
2025-04-27 20:52:39 +0300

    Add comments and future TODOs
    Took 1 hour 1 minute

--- main.ts
+++ main.ts
@@ -3,3 +3,6 @@
 
 // |= a model makes true, with a given predicate; M |= P
 
+// TODO: Work on parsing DIMACS format
+// TODO: Work on DPLL core
+
--- types.ts
+++ types.ts
@@ -5,28 +5,26 @@
 
     A negative value means a negative literal, aka -2 -> !x2
 */
-type Literal = {
-    value: number;
-}
+
+type Literal = number;
 
 /*
-    A list of literals.
+    A set of literals. We uset set over list for performance:
+    you can't have 2 literals in the same list or even opposite.
  */
-type Clause = {
-    value: Literal[];
-}
 
-
-
+type Clause = Set<Literal>;
 
 // CNF: ^ intre ()
 
-type CnfFormula = {
-    value: Clause[];
-}
+type CnfFormula = Clause[];
 
 /*
     The way we would assign values to literals.
+    We will use a map due to its performance for adding and removing
+    elements from it.
  */
+
+
 type Assign = Map<number, boolean>;
 
